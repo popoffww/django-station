e3dbc80b251e99d195cdc50e9f2cb16eac0b7ced
popoffww <popoffww@yandex.ru>
2020-09-04 11:31:40 +0600

    Initial commit

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -20,25 +23,28 @@ class CityDetailView(DetailView):
     context_object_name = 'object'
     template_name = 'cities/detail.html'
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model= City
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('city:home')
+    success_message = 'Город успешно создан'
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model= City
     form_class = CityForm
     template_name = 'cities/update.html'
     success_url = reverse_lazy('city:home')
+    success_message = 'Город успешно изменен'
 
 class CityDeleteView(DeleteView):
     model= City
     template_name = 'cities/delete.html'
     success_url = reverse_lazy('city:home')
 
-    # Удаление без использования template_name
-    # Удаляет не спрашивая
+    # Закомментировать template_name
+    # Удаляет не спрашивая, с выводом сообщения об удалении
 
     # def get(self, request, *args, **kwargs):
+    #     messages.success(request, 'Город успешно удален')
     #     return self.post(request, *args, **kwargs)
