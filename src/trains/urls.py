1a80308760e0fd76e6041a5ae1bfa372ab5e274f
popoffww <popoffww@yandex.ru>
2020-09-05 15:30:44 +0600

    Initial commit

--- src/trains/urls.py
+++ src/trains/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import home
-from .views import TrainCreateView
+from .views import home, TrainCreateView, TrainUpdateView, TrainDeleteView, TrainDetailView
 
 urlpatterns = [
     path('', home, name='home'),
-    # path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
-    # path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
-    # path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
+    path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
     path('add/', TrainCreateView.as_view(), name='add'),
 ]
--- src/trains/views.py
+++ src/trains/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import Train
@@ -14,9 +15,26 @@ def home(request):
     trains = paginator.get_page(page_number)
     return render(request, 'trains/home.html', {'objects_list': trains})
 
+class TrainDetailView(DetailView):
+    queryset = Train.objects.all()
+    context_object_name = 'object'
+    template_name = 'trains/detail.html'
+
 class TrainCreateView(SuccessMessageMixin, CreateView):
     model = Train
     form_class = TrainForm
     template_name = 'trains/create.html'
     success_url = reverse_lazy('train:home')
     success_message = 'Маршрут успешно создан'
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/update.html'
+    success_url = reverse_lazy('train:home')
+    success_message = 'Маршрут успешно изменен'
+
+class TrainDeleteView(DeleteView):
+    model= Train
+    template_name = 'trains/delete.html'
+    success_url = reverse_lazy('train:home')
