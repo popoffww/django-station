13a24293b8dd8aaa7f3510aa943cbee6ef8ff811
popoffww <popoffww@yandex.ru>
2020-09-06 21:26:11 +0600

    Routes App Forms

--- src/routes/views.py
+++ src/routes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import *
 
-# Create your views here.
+def home(request):
+    form = RouteForm
+    return render(request, 'routes/home.html', {'form': form})
+
+
--- src/voyage/urls.py
+++ src/voyage/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view
+from routes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
+    path('', home, name='home'),
     path('cities/', include(('cities.urls', 'city'))),
     path('trains/', include(('trains.urls', 'train'))),
 ]
