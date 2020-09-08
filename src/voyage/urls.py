57dfb8191998af27877f7731a11dc80d70dc0387
popoffww <popoffww@yandex.ru>
2020-09-08 13:18:20 +0600

    App Finished

--- src/voyage/urls.py
+++ src/voyage/urls.py
@@ -15,13 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from routes.views import home
+from routes.views import (home, find_routes, add_route,
+                          RouteListView, RouteDetailView, RouteDeleteView)
+from .views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
     path('cities/', include(('cities.urls', 'city'))),
     path('trains/', include(('trains.urls', 'train'))),
+    path('find/', find_routes, name='find_routes'),
+    path('add_route/', add_route, name='add_route'),
+    path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', RouteDeleteView.as_view(), name='delete'),
+    path('', home, name='home'),
+
 ]
 
 
